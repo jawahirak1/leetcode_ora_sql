18d2deac2a1a987c32b47dac947aa67e6feb2765
jawahirak1 <54602999+jawahirak1@users.noreply.github.com>
2019-09-02 16:49:23 -0600

    Create Schema.sql

---
+++ 176_Second_Highest_Salary/Schema.sql
@@ -0,0 +1,8 @@
+-- Oracle Table Creation Script
+-- Author: Jawahir Kasim
+Create table Employee (Id int, Salary int);
+Truncate table Employee;
+insert into Employee (Id, Salary) values ('1', '100');
+insert into Employee (Id, Salary) values ('2', '200');
+insert into Employee (Id, Salary) values ('3', '300');
+-- EOF
