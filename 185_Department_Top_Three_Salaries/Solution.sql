a105332498ee8cccf3a8971be64f8ec877809afe
jawahirak1 <54602999+jawahirak1@users.noreply.github.com>
2019-09-03 10:06:06 -0600

    Create Solution.sql

---
+++ 185_Department_Top_Three_Salaries/Solution.sql
@@ -0,0 +1,13 @@
+-- 
+-- Author: Jawahir Kasim
+-- 
+select d.Name as "Department"
+, empranksal.Name as "Employee"
+, empranksal.Salary as "Salary" 
+from
+(select id, name, salary
+, dense_rank () over (partition by departmentid order by salary desc) rank_by_dept
+, departmentId 
+from Employee) empranksal, department d
+where d.id = empranksal.departmentId
+and empranksal.rank_by_dept <= 3
